85c893fa86aca4bb3803b29ba1e8a48393e09f51
zl <zl9@meitu.com>
2019-12-23 17:16:28 +0800

    no message

--- datasets/coco.py
+++ datasets/coco.py
@@ -86,7 +86,8 @@ class COCODetection(data.Dataset):
                  target_transform=COCOAnnotationTransform(), dataset_name='MS COCO'):
         sys.path.append(osp.join(root, COCO_API))
         from pycocotools.coco import COCO
-        self.root = osp.join(root, IMAGES, image_set)
+        # self.root = osp.join(root, IMAGES, image_set)
+        self.root = osp.join(root, image_set)
         self.coco = COCO(osp.join(root, ANNOTATIONS,
                                   INSTANCES_SET.format(image_set)))
         self.ids = list(self.coco.imgToAnns.keys())
